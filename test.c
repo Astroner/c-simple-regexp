ad0dc42674e8b4a5b33b9f27bd626fdeae458598
Evgeny Nikiforov <enikiforov@devexperts.com>
2023-03-29 01:56:28 +0300

    test improvement

--- test.c
+++ test.c
@@ -4,12 +4,16 @@
 #include "RegExpLib.h"
 
 int main(void) {
+    RegExpSearchHit hit;
+
     char* string = "Mem of the year: LulW";
     char* regexp = ": \\w\\w\\w\\w";
 
-    int hits = RegExp_search(regexp, string, NULL);
+    int hits = RegExp_search(regexp, string, &hit);
 
     assert(hits == 1);
+    assert(hit.start == 15);
+    assert(hit.length == 6);
 
     return 0;
 }
